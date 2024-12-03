f3824a17a3327f0d4c1cdbe23464aed66f6d680b
Lovish Duggal <lovishduggal11@gmail.com>
2024-12-03 17:30:00 +0530

    fix: add ESLint disable comment for unused variable in MultiSelectorInput component

--- components/ui/multi-select.tsx
+++ components/ui/multi-select.tsx
@@ -270,6 +270,7 @@ MultiSelectorTrigger.displayName = 'MultiSelectorTrigger';
 const MultiSelectorInput = forwardRef<
   React.ElementRef<typeof CommandPrimitive.Input>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Input>
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
 >(({ className, ...props }, ref) => {
   const {
     setOpen,
